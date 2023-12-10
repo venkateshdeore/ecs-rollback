d3a2762b9d49bdccf39bfc7d0dbb71fca2618d7d
codebreaker343 <deore.venkatesh@gmail.com>
2023-12-10 15:55:29 +0530

    Misc formatting

--- ecs_rollback.py
+++ ecs_rollback.py
@@ -1,11 +1,15 @@
-import click 
+import click
 
 from app.app import App
 
-@click.command()
-@click.option('-c', '--cluster', type=str, help='Name of the AWS ECS cluster', required=True)
-@click.option('-s', '--service', type=str, help='Name of the AWS ECS service', required=True)
-@click.option('-r', '--region', type=str, help='Name of the AWS region', required=True)
 
+@click.command()
+@click.option(
+    "-c", "--cluster", type=str, help="Name of the AWS ECS cluster", required=True
+)
+@click.option(
+    "-s", "--service", type=str, help="Name of the AWS ECS service", required=True
+)
+@click.option("-r", "--region", type=str, help="Name of the AWS region", required=True)
 def cli(cluster, service, region):
     App(cluster=cluster, service=service, region=region).rollback()
